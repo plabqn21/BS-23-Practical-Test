7afa4e8a05e48aa8998513a760a05ed4edc9c37a
Plabqn <plabqncsejnu@gmail.com>
2020-12-04 14:47:48 +0600

    Add project files.

---
+++ BS-23-PracticalTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BS_23_PracticalTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
